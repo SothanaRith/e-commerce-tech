d12b6913dd9f55d336b1b56dee378db63abaae63
narithR <sothanarithheang@gmail.com>
2025-03-04 14:35:12 +0700

    add screen

---
+++ lib/screen/location_page/location_select_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LocationSelectScreen extends StatefulWidget {
+  const LocationSelectScreen({super.key});
+
+  @override
+  State<LocationSelectScreen> createState() => _LocationSelectScreenState();
+}
+
+class _LocationSelectScreenState extends State<LocationSelectScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
---
+++ lib/screen/track_order_page/track_order_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TrackOrderScreen extends StatefulWidget {
+  const TrackOrderScreen({super.key});
+
+  @override
+  State<TrackOrderScreen> createState() => _TrackOrderScreenState();
+}
+
+class _TrackOrderScreenState extends State<TrackOrderScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
